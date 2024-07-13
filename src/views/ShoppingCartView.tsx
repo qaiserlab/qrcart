71b1eb42c5f3cbeec19b031a46b318f0328886f7
Fadlun Anaturdasa <f.anaturdasa@gmail.com>
2024-07-14 06:20:12 +0700

    feat(formik): add formik

--- src/views/ShoppingCartView.tsx
+++ src/views/ShoppingCartView.tsx
@@ -1,36 +1,107 @@
 "use client"
 
 import { useState } from "react"
-import { Button, Col, Form, Input, Modal, ModalBody, ModalFooter, ModalHeader, Row, Table } from "reactstrap"
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Row,
+  Table,
+} from "reactstrap"
+import { Formik, useField, useFormik } from "formik"
+
+function MyInput({ ...props }) {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+
+  // which we can spread on <input>. We can use field meta to show an error
+
+  // message if the field is invalid and it has been touched (i.e. visited)
+
+  // @ts-ignore
+  const [field, meta] = useField(props)
+
+  return (
+    <>
+      <Input type="text" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  )
+}
 
 function AddItemModal(props: TAddItemModalProps) {
+  // const formik = useFormik({
+  //   initialValues: {
+  //     sn: "",
+  //     product: "",
+  //     price: 0,
+  //   },
+
+  //   onSubmit: (values) => {
+  //     alert(JSON.stringify(values, null, 2))
+  //   },
+  // })
+
+  const initialValues = {
+    sn: "",
+    product: "",
+    price: 0,
+  }
+
+  const handleSubmit = (values:any, { setSubmitting }:any) => {
+
+    setTimeout(() => {
+
+      alert(JSON.stringify(values, null, 2));
+
+      setSubmitting(false);
+
+    }, 400);
+
+  }
+  
+
   return (
     <Modal isOpen={props.display} toggle={props.onClose}>
-      <ModalHeader toggle={props.onClose}>Add Item</ModalHeader>
-      <ModalBody>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         <Form>
-          <Row>
-            <Col md={2}>#</Col>
-            <Col><Input type="text" /></Col>
-          </Row>
-          <Row>
-            <Col md={2}>Product</Col>
-            <Col><Input type="text" disabled /></Col>
-          </Row>
-          <Row>
-            <Col md={2}>Price</Col>
-            <Col><Input type="text" disabled /></Col>
-          </Row>
+          <ModalHeader toggle={props.onClose}>Add Item</ModalHeader>
+          <ModalBody>
+            <Row>
+              <Col md={2}>#</Col>
+              <Col>
+                <MyInput name="sn" type="sn" />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={2}>Product</Col>
+              <Col>
+                <Input type="text" disabled />
+              </Col>
+            </Row>
+            <Row>
+              <Col md={2}>Price</Col>
+              <Col>
+                <Input type="text" disabled />
+              </Col>
+            </Row>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" type="submit">
+              Add
+            </Button>{" "}
+            <Button color="secondary" onClick={props.onClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
         </Form>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="primary" onClick={() => props.onClose()}>
-          Add
-        </Button>{' '}
-        <Button color="secondary" onClick={props.onClose}>
-          Cancel
-        </Button>
-      </ModalFooter>
+      </Formik>
     </Modal>
   )
 }
@@ -45,8 +116,13 @@ export default function ShoppingCartView() {
 
       <div style={{ textAlign: "right", paddingBottom: 16 }}>
         <Button color="primary">Scan QRCode</Button>&nbsp;
-        <Button color="primary" onClick={() => setDisplayAddItem(true)}>Add Item</Button>&nbsp;
-        <Button color="success" onClick={() => setDisplayModal(true)}>Pay</Button>
+        <Button color="primary" onClick={() => setDisplayAddItem(true)}>
+          Add Item
+        </Button>
+        &nbsp;
+        <Button color="success" onClick={() => setDisplayModal(true)}>
+          Pay
+        </Button>
       </div>
 
       <Table bordered>
@@ -81,30 +157,39 @@ export default function ShoppingCartView() {
         </tfoot>
       </Table>
 
-      <AddItemModal display={displayAddItem} onClose={() => setDisplayAddItem(false)} />
+      <AddItemModal
+        display={displayAddItem}
+        onClose={() => setDisplayAddItem(false)}
+      />
 
       <Modal isOpen={displayModal} toggle={() => setDisplayModal(false)}>
         <ModalHeader toggle={() => setDisplayModal(false)}>QRCode</ModalHeader>
         <ModalBody>
-          <table style={{ margin: '0 auto' }}>
+          <table style={{ margin: "0 auto" }}>
             <tr>
               <td style={{ width: 100, height: 50 }}>Total</td>
-              <td><Input type="text" style={{ width: 320 }} disabled /></td>
+              <td>
+                <Input type="text" style={{ width: 320 }} disabled />
+              </td>
             </tr>
             <tr>
               <td style={{ width: 100, height: 50 }}>Pay</td>
-              <td><Input type="text" style={{ width: 320 }} /></td>
+              <td>
+                <Input type="text" style={{ width: 320 }} />
+              </td>
             </tr>
             <tr>
               <td style={{ width: 100, height: 50 }}>Change</td>
-              <td><Input type="text" style={{ width: 320 }} disabled /></td>
+              <td>
+                <Input type="text" style={{ width: 320 }} disabled />
+              </td>
             </tr>
           </table>
         </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={() => setDisplayModal(false)}>
             Save
-          </Button>{' '}
+          </Button>{" "}
           <Button color="secondary" onClick={() => setDisplayModal(false)}>
             Cancel
           </Button>
